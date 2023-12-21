002ef5c01f35c9f831b32d5ab8d74dae7e6c7961
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2023-12-21 18:17:07 +0900

    fix: 一応nullセーフにする

--- src/main/java/com/github/hibi_10000/plugins/joinplus/ConfigUtil.java
+++ src/main/java/com/github/hibi_10000/plugins/joinplus/ConfigUtil.java
@@ -1,10 +1,9 @@
 package com.github.hibi_10000.plugins.joinplus;
 
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-
-import java.util.Objects;
 
 public class ConfigUtil {
     private ConfigUtil() {}
@@ -64,7 +63,8 @@ public class ConfigUtil {
 
     private static String getString(final String path) {
         String str = config.getString(path);
-        if (str != null && str.isEmpty()) str = config.getDefaults().getString(path);
+        Configuration defaults = config.getDefaults();
+        if (str != null && str.isEmpty() && defaults != null) str = defaults.getString(path);
         return str;
     }
 
